17785e88f24e3b6f8ecc76e39691febaea6d2dba
itamarh1999 <itamarh19@gmail.com>
2025-01-15 22:13:46 +0200

    maybe this time

--- client.py
+++ client.py
@@ -50,7 +50,7 @@ class SpeedTestClient:
     def listen_for_offer(self):
         # Create UDP socket for listening to offers
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(('', 13117))
 
         while True:
